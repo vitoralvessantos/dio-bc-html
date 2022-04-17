4ef968973d1e1bf35440dd234b9333435df02d6f
vitoralvessantos <vtralves.santos@gmail.com>
2022-04-17 17:49:22 -0300

    create food snake

--- Projetos/SnakeGameJS/js/script.js
+++ Projetos/SnakeGameJS/js/script.js
@@ -7,29 +7,64 @@ snake[0] = {
     y:8*box
 }
 let direction ="right";
+let food ={
+    x: Math.floor(Math.random()*15 +1)*box,
+    y: Math.floor(Math.random()*15 +1)*box
+}
 
 
 function criarBG(){
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16*box, 16*box);
+    context.fillRect(0, 0, 16*box, 16*box); /*Cordenada*/
 }
 
 function snakeCreate(){
     for(i=0; i< snake.length; i++){
         context.fillStyle = "green";
+        
+        /*strokeStyle = "black";*/
         context.fillRect(snake[i].x, snake[i].y, box, box);
-
     }
 }
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(e){
+    if(e.keyCode == 37 && direction != "right") direction = "left";
+    if(e.keyCode == 38 && direction != "down") direction = "up";
+    if(e.keyCode == 39 && direction != "left") direction = "right";
+    if(e.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15*box && direction == 'right') snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 15*box;
+    if(snake[0].y > 16*box && direction == 'down') snake[0].y= 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16*box;
+
     criarBG();
     snakeCreate();
+    drawFood();
+
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
+
     if(direction =="right") snakeX +=box;
     if(direction =="left") snakeX -=box;
     if(direction =="up") snakeY -=box;
-    if(direction =="down") snakeX +=box;
+    if(direction =="down") snakeY +=box;
+
+    if(snakeX != food.x || snakeY !=y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random()*15 +1)*box,
+        food.y = Math.floor(Math.random()*15 +1)*box
+    }
 
     snake.pop();
 
@@ -37,10 +72,10 @@ function iniciarJogo(){
         x: snakeX,
         y: snakeY
     }
+    snake.unshift(newHead);
 
-    
 
 }
 
-let jogo = setInterval(iniciarJogo,100);
+let jogo = setInterval(iniciarJogo,120);
 
